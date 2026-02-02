45abea9c441478a05732c1de5f4dfa6c93352811
anuragmaganti <amaganti.dev@gmail.com>
2026-02-02 00:04:26 -0500

    add model tracking to video frames to detect if hand is present

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,46 @@ function App() {
 
   const handLandmarkerRef = useRef(null);
   const videoRef = useRef(null);
+  const intervalRef = useRef(null);
+  const canvasRef = useRef(null);
 
   const [cameraOn , setCameraOn] = useState(false);
   const [modelReady, setModelReady] = useState(false);
+  const [handDetected, setHandDetected] = useState(false);
 
   useEffect (()=> {
     async function init() {
 
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.21/wasm"
-    );
+      const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.21/wasm"
+      );
 
-    const handLandmarker = await HandLandmarker.createFromOptions(vision, {
-      baseOptions: { modelAssetPath: MODEL_URL },
-      runningMode: "VIDEO",
-      numHands: 1,
-    });
+      const handLandmarker = await HandLandmarker.createFromOptions(vision, {
+        baseOptions: { modelAssetPath: MODEL_URL },
+        runningMode: "VIDEO",
+        numHands: 1,
+      });
 
-    handLandmarkerRef.current = handLandmarker;
-    console.log("HandLandmarker ready");
-    setModelReady(true);
+      handLandmarkerRef.current = handLandmarker;
+      console.log("HandLandmarker ready");
+      setModelReady(true);
 
     }
 
     init();
 
   }, []);
+
+  function startDetectionInterval() {
+    const video = videoRef.current;
+    const landmarker = handLandmarkerRef.current;
+
+    return setInterval(() => {
+      const result = landmarker.detectForVideo(video, performance.now());
+      setHandDetected(result.landmarks.length > 0);
+    }, 200);
+
+  }
 
   async function startCamera() {
     const stream = await navigator.mediaDevices.getUserMedia( {video:true});
@@ -42,10 +56,19 @@ function App() {
     video.srcObject = stream;
     await video.play();
 
-    setCameraOn(true);
+    setCameraOn(true)
+    intervalRef.current = startDetectionInterval();
   }
 
   function stopCamera() {
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+
+    setHandDetected(false);
+
     const video = videoRef.current;
     const stream = video?.srcObject;
 
@@ -59,7 +82,7 @@ function App() {
 
   return (
     <div>
-      <video ref={videoRef} muted playsInline style={{background:"black", objectFit: "cover"}}></video>
+      <video ref={videoRef} muted playsInline style={{background:"black", objectFit: "cover", transform: "scaleX(-1)"}}></video>
       <div>
       {!cameraOn ? (
           <button onClick={startCamera} disabled={!modelReady}>
@@ -68,8 +91,12 @@ function App() {
         ) : (
           <button onClick={stopCamera}>Stop Camera</button>
         )}
-        <span>Model: {modelReady ? "ready ✅" : "loading..."}</span>
+        <span>Model: {modelReady ? "ready" : "loading"}</span>
+        <br></br>
+        <span>Hand: {handDetected ? "detected" : "no hand detected"}</span>
       </div>
+
+      <canvas ref={canvasRef} style={{background: "gray", transform: "scaleX(-1)", width: "500px", height: "500px"}}></canvas>
 
 
     </div>

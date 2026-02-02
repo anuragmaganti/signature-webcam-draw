3211a1bee93aa6ec4f6e9a819e02ee861cb09559
anuragmaganti <amaganti.dev@gmail.com>
2026-02-01 21:28:07 -0500

    load model into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef } from 'react';
 import './App.css'
+import { FilesetResolver, HandLandmarker } from "@mediapipe/tasks-vision";
+
+const MODEL_URL = "/models/hand_landmarker.task";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const handLandmarkerRef = useRef(null);
+
+  useEffect(()=> {
+    async function init() {
+
+    const vision = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    );
+
+    const handLandmarker = await HandLandmarker.createFromOptions(vision, {
+      baseOptions: { modelAssetPath: MODEL_URL },
+      runningMode: "VIDEO",
+      numHands: 1,
+    });
+
+    handLandmarkerRef.current = handLandmarker;
+    console.log("HandLandmarker ready");
+
+    }
+
+    init();
+
+  }, []);
+
+  return <h1>Pinch Pop</h1>
+
 }
+
 
 export default App

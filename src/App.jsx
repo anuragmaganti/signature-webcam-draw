e8c7b6772645cb3d34cc79e84e557b0510163830
anuragmaganti <amaganti.dev@gmail.com>
2026-02-01 23:37:41 -0500

    add webcam preview and button to start and stop it. add cameraOn and mo delReady states to track if either loads

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css'
 import { FilesetResolver, HandLandmarker } from "@mediapipe/tasks-vision";
 
@@ -7,12 +7,16 @@ const MODEL_URL = "/models/hand_landmarker.task";
 function App() {
 
   const handLandmarkerRef = useRef(null);
+  const videoRef = useRef(null);
 
-  useEffect(()=> {
+  const [cameraOn , setCameraOn] = useState(false);
+  const [modelReady, setModelReady] = useState(false);
+
+  useEffect (()=> {
     async function init() {
 
     const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.21/wasm"
     );
 
     const handLandmarker = await HandLandmarker.createFromOptions(vision, {
@@ -23,6 +27,7 @@ function App() {
 
     handLandmarkerRef.current = handLandmarker;
     console.log("HandLandmarker ready");
+    setModelReady(true);
 
     }
 
@@ -30,7 +35,45 @@ function App() {
 
   }, []);
 
-  return <h1>Pinch Pop</h1>
+  async function startCamera() {
+    const stream = await navigator.mediaDevices.getUserMedia( {video:true});
+    const video = videoRef.current;
+
+    video.srcObject = stream;
+    await video.play();
+
+    setCameraOn(true);
+  }
+
+  function stopCamera() {
+    const video = videoRef.current;
+    const stream = video?.srcObject;
+
+    if (stream && stream.getTracks) {
+      stream.getTracks().forEach((track) => track.stop());
+    }
+
+    if (video) video.srcObject = null;
+    setCameraOn(false);
+  }
+
+  return (
+    <div>
+      <video ref={videoRef} muted playsInline style={{background:"black", objectFit: "cover"}}></video>
+      <div>
+      {!cameraOn ? (
+          <button onClick={startCamera} disabled={!modelReady}>
+            Start Camera
+          </button>
+        ) : (
+          <button onClick={stopCamera}>Stop Camera</button>
+        )}
+        <span>Model: {modelReady ? "ready ✅" : "loading..."}</span>
+      </div>
+
+
+    </div>
+  )
 
 }
 
